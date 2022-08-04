8827687103f61c14174efa6f94fb9ffedb6533ec
Yassir Ah <yassirnugroho48@gmail.com>
2022-08-05 06:54:59 +0700

    Create Menu Component

---
+++ src/Components/Navigation.js
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+function Navigation() {
+	const [showMenu, setShowMenu] = useState(false);
+
+	let menu;
+	let menuMask;
+
+	if (showMenu) {
+		menu = (
+			<div className=" fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
+				The Menu
+			</div>
+		);
+
+		menuMask = (
+			<div
+				className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+				onClick={() => setShowMenu(false)}
+			></div>
+		);
+	}
+
+	return (
+		<nav>
+			<span className="text-xl">
+				<FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
+
+				{menuMask}
+
+				{menu}
+			</span>
+		</nav>
+	);
+}
+
+export default Navigation;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				"black-t-50": "rgba(0,0,0,0.5)",
+			},
+		},
 	},
 	plugins: [],
 };
