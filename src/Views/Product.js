2b02b1d4ad31525c3d03b5b9a85c911f45e24e07
Yassir Ah <yassirnugroho48@gmail.com>
2022-08-05 23:34:48 +0700

    fetching data from API

---
+++ src/Views/Product.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Product() {
+	const { id } = useParams();
+	const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`;
+	const [product, setProduct] = useState(null);
+
+	let content = null;
+
+	useEffect(() => {
+		axios.get(url).then((response) => {
+			setProduct(response.data);
+		});
+	}, [url]);
+
+	if (product) {
+		content = (
+			<div>
+				<h1 className="text-2xl font-bold mb-3">{product.name}</h1>
+				<div>
+					<img src={product.images[0].imageUrl} alt={product.name} />
+				</div>
+				<div className="font-bold text-xl mb-4">$ {product.price}</div>
+				<div>{product.description}</div>
+			</div>
+		);
+	}
+
+	return <div>{content}</div>;
+}
+
+export default Product;
