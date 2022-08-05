caa58d4ceeedfdd341647828e741cb95a599eb04
Yassir Ah <yassirnugroho48@gmail.com>
2022-08-05 15:51:45 +0700

    Animate the Menu

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,37 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import { useTransition, animated } from "react-spring";
 
 function Navigation() {
 	const [showMenu, setShowMenu] = useState(false);
 
-	let menu;
-	let menuMask;
+	const maskTransitions = useTransition(showMenu, {
+		from: { opacity: 0, position: "absolute" },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
+	});
 
-	if (showMenu) {
-		menu = (
-			<div className=" fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
-				The Menu
-			</div>
-		);
-
-		menuMask = (
-			<div
-				className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-				onClick={() => setShowMenu(false)}
-			></div>
-		);
-	}
+	const menuTransitions = useTransition(showMenu, {
+		from: { opacity: 0, transform: "translateX(-100%)" },
+		enter: { opacity: 1, transform: "translateX(0%)" },
+		leave: { opacity: 0, transform: "translateX(-100%)" },
+	});
 
 	return (
 		<nav>
 			<span className="text-xl">
 				<FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
-
-				{menuMask}
-
-				{menu}
 			</span>
+
+			{maskTransitions(
+				(styles, item) =>
+					item && (
+						<animated.div
+							style={styles}
+							className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+							onClick={() => setShowMenu(false)}
+						>
+							This is the Menu
+						</animated.div>
+					)
+			)}
+
+			{menuTransitions(
+				(styles, item) =>
+					item && (
+						<animated.div
+							style={styles}
+							className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+						>
+							<span>The Menu</span>
+							<ul>
+								<li>Home</li>
+							</ul>
+						</animated.div>
+					)
+			)}
 		</nav>
 	);
 }
