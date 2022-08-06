dc7b367324c6dd9e2b7e8d21f825fb4445989f10
Yassir Ah <yassirnugroho48@gmail.com>
2022-08-06 08:17:31 +0700

    loader animation and handle an error failed req

---
+++ src/Components/Loader.js
@@ -0,0 +1,9 @@
+function Loader() {
+	return (
+		<div className="flex justify-center">
+			<div className="loader"></div>
+		</div>
+	);
+}
+
+export default Loader;
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,29 +1,63 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Loader from "../Components/Loader";
 
 function Product() {
 	const { id } = useParams();
 	const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`;
-	const [product, setProduct] = useState(null);
+	const [product, setProduct] = useState({
+		loading: false,
+		data: null,
+		error: false,
+	});
 
 	let content = null;
 
 	useEffect(() => {
-		axios.get(url).then((response) => {
-			setProduct(response.data);
+		setProduct({
+			loading: true,
+			data: null,
+			error: false,
 		});
+
+		axios
+			.get(url)
+			.then((response) => {
+				setProduct({
+					loading: false,
+					data: response.data,
+					error: false,
+				});
+			})
+			.catch((error) => {
+				setProduct({
+					loading: false,
+					data: null,
+					error: true,
+				});
+			});
 	}, [url]);
 
-	if (product) {
+	if (product.error) {
+		content = (
+			<p>There was an error loading the product please try again later</p>
+		);
+	}
+
+	if (product.loading) {
+		content = <Loader />;
+	}
+
+	if (product.data) {
 		content = (
 			<div>
-				<h1 className="text-2xl font-bold mb-3">{product.name}</h1>
+				<h1 className="text-2xl font-bold mb-3">{product.data.name}</h1>
 				<div>
-					<img src={product.images[0].imageUrl} alt={product.name} />
+					<img src={product.data.images[0].imageUrl} alt={product.data.name} />
 				</div>
-				<div className="font-bold text-xl mb-4">$ {product.price}</div>
-				<div>{product.description}</div>
+				<div className="font-bold text-xl mb-4">$ {product.data.price}</div>
+				<div>{product.data.description}</div>
 			</div>
 		);
 	}
